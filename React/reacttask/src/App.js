5057fc7d4fbb4ef61ef54bc2d7b3bdd06ceb1415
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-06-06 18:27:00 +0530

    first commit

---
+++ React/reacttask/src/App.js
@@ -0,0 +1,12 @@
+import Class_component from "./Component/Class_component"
+import Function_component from "./Component/Function_component"
+
+function App() {
+  return (
+    <div>
+        <Function_component/>
+        <Class_component/>
+    </div>
+  )
+}
+export default App
