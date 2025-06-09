1037863ce61de71a95b8b28fffbcf0ef61889b70
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-06-09 17:54:57 +0530

    first commit

--- React/reacttask/src/App.js
+++ React/reacttask/src/App.js
@@ -1,11 +1,19 @@
 import Class_component from "./Component/Class_component"
 import Function_component from "./Component/Function_component"
+import Jsx from "./JSX/Jsx"
+import Main_layout from "./Layout/Main_layout"
 
 function App() {
   return (
     <div>
-        <Function_component/>
-        <Class_component/>
+      {
+        //<Function_component/>
+        //<Class_component/>
+
+        //<Jsx/>
+        <Main_layout/>
+
+      }
     </div>
   )
 }
