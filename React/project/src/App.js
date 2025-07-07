1ff8252b3ed16e548fe8509777e8cc3e777f54c5
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-07-07 18:22:37 +0530

    first

--- React/project/src/App.js
+++ React/project/src/App.js
@@ -7,6 +7,14 @@ import Properties_details from "./website/pages/Properties_details";
 import Contact from "./website/pages/Contact";
 import Signup from "./website/pages/Signup";
 import Login from "./website/pages/Login";
+import AHeader from "./admin/component/AHeader";
+import AFooter from "./admin/component/AFooter";
+import Admin_login from "./admin/pages/Admin_login";
+import Dashboard from "./admin/pages/Dashboard";
+import Add_categories from "./admin/pages/Add_categories";
+import Manage_categories from "./admin/pages/Manage_categories";
+import Add_properties from "./admin/pages/Add_properties";
+import Manage_properties from "./admin/pages/Manage_properties";
 
 function App() {
   return (
@@ -21,6 +29,15 @@ function App() {
           <Route path="/login" element={<> <Header/> <Login/> <Footer/></>}></Route>
 
           
+
+          <Route path="/admin-login" element={<>  <Admin_login/> <AFooter/></>}></Route>
+          <Route path="/dashboard" element={<> <AHeader/> <Dashboard/> <AFooter/></>}></Route>
+
+          <Route path="/add_categories" element={<> <AHeader/> <Add_categories/> <AFooter/></>}></Route>
+          <Route path="/manage_categories" element={<> <AHeader/> <Manage_categories/> <AFooter/></>}></Route>
+
+          <Route path="/add_properties" element={<> <AHeader/> <Add_properties/> <AFooter/></>}></Route>
+          <Route path="/manage_properties" element={<> <AHeader/> <Manage_properties/> <AFooter/></>}></Route>
 
         </Routes>
       </BrowserRouter>
