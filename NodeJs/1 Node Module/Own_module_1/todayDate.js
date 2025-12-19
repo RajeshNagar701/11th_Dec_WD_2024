9014a13b0657f46368f6f2946d7acbb598b6583e
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-12-19 18:43:16 +0530

    first commit

---
+++ NodeJs/1 Node Module/Own_module_1/todayDate.js
@@ -0,0 +1,14 @@
+
+
+
+// this my custome own /local module
+
+var todayDate=()=>{
+	
+	var today = new Date();
+	var date = (today.getDate())+'-'+ (today.getMonth()+1) +'-'+(today.getFullYear());
+	return date;
+}
+
+// local module export
+module.exports = todayDate ;
